8029456f364d01950b17c3a8c85c2b6b01c2901b
Jayakumarkjk <115686350+Jayakumarkjk@users.noreply.github.com>
2023-04-22 09:27:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+let selectedRow = null;
+
+function onFormSubmit(){
+
+    var formData=readFormData();
+
+    if(selectedRow == null) insertNewrecord(formData);
+    else updateRecord(formData);
+
+}
+
+var checkboxes=document.querySelectorAll('.checkbox');
+let arr=[];
+for(var checkbox of checkboxes){
+    checkbox.addEventListener('click',function(){
+        if(this.checked == true){
+            arr.push(this.value);
+        }else{
+           
+            arr=arr.filter(e =>(e!=this.value));
+        }
+    })
+}
+function readFormData(){
+    const formData= {};
+    formData["firstName"]=     document.getElementById("first-name").value;
+    formData["lastName"] =     document.getElementById("last-name").value;
+    formData["address"]  =     document.getElementById("address").value;
+    formData["pincode"]  =     document.getElementById("pincode").value;
+    formData["gender"]   =     document.getElementById("gender").value;
+    formData["food"]     =     arr.join(',');
+    formData["state"]    =     document.getElementById("state").value;
+    formData["country"]  =     document.getElementById("country").value;
+    return formData;
+
+}
+
+function insertNewrecord(data){
+    let table=document.getElementById("employeeList").getElementsByTagName("tbody")[0];
+    console.log(table);
+
+    var newRow=table.insertRow(table.length);
+    cell1=newRow.insertCell(0);
+    cell1.innerHTML=data.firstname;
+    cell2=newRow.insertCell(1);
+    cell2.innerHTML=data.lastname;
+    cell3=newRow.insertCell(2);
+    cell3.innerHTML=data.address;
+    cell4=newRow.insertCell(3);
+    cell4.innerHTML=data.pincode;
+    cell5=newRow.insertCell(4);
+    cell5.innerHTML=data.gender;
+    cell6=newRow.insertCell(5);
+    cell6.innerHTML=data.food;
+    cell7=newRow.insertCell(6);
+    cell7.innerHTML=data.state;
+    cell8=newRow.insertCell(7);
+    cell8.innerHTML=data.country;
+    cell9=newRow.insertCell(8);
+    cell9.innerHTML=`
+    <button class="btn btn-secondary" onClick="onDelete(this)"><i class="bi bi-trash-fill"></i></button>
+    `
+}
+
+function resetForm(){
+    document.getElementById("first-name").value="";
+    document.getElementById("last-name").value="";
+    document.getElementById("address").value="";
+    document.getElementById("pincode").value="";
+    document.getElementById("gender").value="";
+    document.getElementById("state").value="";
+    document.getElementById("country").value="";
+}
+
+function updateRecord(formData){
+    console.log(formData);
+    selectedRow.cells[0].innerHTML=formData.firstname;
+    selectedRow.cells[1].innerHTML=formData.lastname;
+    selectedRow.cells[2].innerHTML=formData.address;
+    selectedRow.cells[3].innerHTML=formData.pincode;
+    selectedRow.cells[4].innerHTML=formData.gender;
+    selectedRow.cells[5].innerHTML=formData.food;
+    selectedRow.cells[6].innerHTML=formData.state;
+    selectedRow.cells[7].innerHTML=formData.country;
+
+}
+
+function onDelete(td) {
+    if (confirm("Do you want to Delete ? ")) {
+        var s=td.parentElement.parentElement;
+        s.parentElement.removeChild(s);
+        resetForm();
+    }
+}
